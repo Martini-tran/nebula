447e9d32dc0361d5d7e9152c74ad6d314d999404
xiangqainlu <lu@cn.com>
2026-05-06 16:42:17 +0800

    feat: 创建

--- nebula-apis/nebula-api-auth/src/main/java/com/nebula/auth/dto/RegisterRequest.java
+++ nebula-apis/nebula-api-auth/src/main/java/com/nebula/auth/dto/RegisterRequest.java
@@ -5,7 +5,13 @@ import lombok.Data;
 @Data
 public class RegisterRequest {
 
+    /**
+     * 用户名
+     */
     private String username;
+    /**
+     * 密码
+     */
     private String password;
 
     /**
